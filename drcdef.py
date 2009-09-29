4e9ab35a97f84c388cde85d48ce8ac2cc91a8bc7
john <johnpdaigle@gmail.com>
2009-09-29 13:06:55 -0400

    split the object off.

--- drcdef.py
+++ drcdef.py
@@ -4,6 +4,8 @@
 # drcdef.py
 # phase 1 grammar for DRC queries
 
+import drcobj
+
 # token names
 
 reserved = {
@@ -56,49 +58,6 @@ def t_error(t):
 # Build the lexer
 import ply.lex as lex
 lex.lex()
-
-#DRC Class
-
-class DRC(object):
-    def __init__(self, nodeType):
-        self.nodeType = nodeType
-        print "%s DRCNode created" % self.nodeType
-        self.predicateName = "tablename"
-        self.children = []
-        self.argList = []
-        self.varList = []
-        self.leftOperand = []
-        self.leftOperandType = []
-        self.operator = []
-        self.rightOperand = []
-        self.rightOperandType = []
-
-    def set_predicate(self, pName):
-        self.predicateName = pName
-
-    def set_children(self, child):
-        self.children.append(child)
-
-    def set_arglist(self, arglist):
-        self.argList.append(arglist)
-
-    def set_varlist(self, varlist):
-        self.varList.append(varlist)
-
-    def set_arglist(self, leftop):
-        self.leftOperand.append(leftop)
-
-    def set_arglist(self, leftop_type):
-        self.leftOperandType.append(leftop_type)
-
-    def set_arglist(self, rightop):
-        self.rightOperand.append(rightop)
-
-    def set_arglist(self, rightop_type):
-        self.rightOperandType.append(rightop_type)
-
-    def set_arglist(self, operator):
-        self.operator.append(operator)
 
 # Dictionary and DRCNode counter
 
