8d0f25ca6d33cb6358ac8d71c5442da5aa890c54
marco <marco@localhost>
2009-10-31 11:45:28 -0400

    DBengine

---
+++ drctokens.py
@@ -0,0 +1,62 @@
+# CSC 6340
+# Fall 2009
+# Term Project
+# Marco Valero, J. Paul Daigle
+
+# module drctokens.py
+# this file only contains the token rules for the Calculus
+
+# token names
+
+reserved = {
+    'and' : 'AND',
+    'or' : 'OR',
+    'not' : 'NOT',
+    'exists' : 'EXISTS',
+    'forall' : 'FORALL',
+    'exit' : 'EXIT',
+    '<=' : 'LESSEQ',
+    '>=' : 'GREAEQ',
+    '<>' : 'DIFFERENT',
+    '<' : 'LESS',
+    '>' : 'GREAT',
+    '=' : 'EQUAL',}
+
+tokens = [
+    'LBRACE', 'RBRACE', 'BAR',
+    'LPAREN', 'RPAREN',
+    'COMMA', 
+    'NAME', 'NUMBER', 'STRING'] + list(reserved.values())
+
+
+#Tokens
+
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_RESERVED(t):
+    r'and | or | exists | forall | not | exit | <= | >= | <> | < | > | ='
+    t.type = reserved.get(t.value, 'BOMB')
+    return t
+
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_BAR    = r'\|'
+t_COMMA  = r','
+t_NAME   = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_STRING = r"\'[^']*\'"
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
